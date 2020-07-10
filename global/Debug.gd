7e7f250235c5ff58e92bd2384b071c670b5fa8c3
henryliu <henryliuser@gmail.com>
2020-07-10 11:29:28 -0700

    debug inputs, global singletons

---
+++ global/Debug.gd
@@ -0,0 +1,15 @@
+extends Node
+var frame_count = 0
+
+func _input(event):
+	if event.is_action_pressed("d_reset"): get_tree().reload_current_scene()
+	if event.is_action_pressed("d_quit"): get_tree().quit()
+	if event.is_action_pressed("d_pause"): get_tree().paused = !get_tree().paused
+	if event.is_action_pressed("d_frame_advance") and get_tree().paused: 
+		get_tree().paused = false
+		yield(get_tree().create_timer(1.0/60, false), "timeout")
+		get_tree().paused = true
+
+func _physics_process(delta):
+	frame_count += 1
+	
