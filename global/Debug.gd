8e95b5102611ed58971d6446321d7d5ac5c4455d
henryliu <henryliuser@gmail.com>
2020-07-10 12:02:26 -0700

    menu

--- global/Debug.gd
+++ global/Debug.gd
@@ -5,6 +5,7 @@ func _input(event):
 	if event.is_action_pressed("d_reset"): get_tree().reload_current_scene()
 	if event.is_action_pressed("d_quit"): get_tree().quit()
 	if event.is_action_pressed("d_pause"): get_tree().paused = !get_tree().paused
+	if event.is_action_pressed("d_menu"): get_tree().change_scene("res://Menu.tscn")
 	if event.is_action_pressed("d_frame_advance") and get_tree().paused: 
 		get_tree().paused = false
 		yield(get_tree().create_timer(1.0/60, false), "timeout")
